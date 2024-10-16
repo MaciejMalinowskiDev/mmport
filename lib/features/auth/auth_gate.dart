23c0f0f1ce160594fd7daee1fcb87149987df9c6
Maciej Malinowski <maciejdevmalinowski@gmail.com>
2024-10-16 10:40:50 +0200

    Add SignIn function

--- lib/features/auth/auth_gate.dart
+++ lib/features/auth/auth_gate.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide EmailAuthProvider;
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mmport/features/home/home_page.dart';
@@ -8,18 +8,20 @@ class AuthGate extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final providers = [EmailAuthProvider()];
    return StreamBuilder<User?>(
     stream: FirebaseAuth.instance.authStateChanges(),
    initialData: FirebaseAuth.instance.currentUser,
    builder: (context, snapshot) {
     if (!snapshot.hasData) {
       return SignInScreen(
-  actions: [
-    AuthStateChangeAction<SignedIn>((context, state) {
-      Navigator.pushReplacementNamed(context, '/home');
-    }),
-  ],
-);
+            providers: providers,
+            actions: [
+              AuthStateChangeAction<SignedIn>((context, state) {
+                Navigator.pushReplacementNamed(context, '/profile');
+              }),
+            ],
+          );
 
     }
    

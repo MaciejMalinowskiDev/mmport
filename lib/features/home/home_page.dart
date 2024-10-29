24c581e5eb26c3c5bf6310df436cca9803650b90
Maciej Malinowski <maciejdevmalinowski@gmail.com>
2024-10-29 20:30:39 +0100

    Add home page with user profile screen

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -1,54 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mmport/features/auth/user_profile.dart';
+import 'package:firebase_ui_firestore/firebase_ui_firestore.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
+class HomePage extends StatelessWidget {
+  HomePage({
+    Key? key,
+    required this.currentUser,
+  }) :super(key: key);
 
-  final String title;
+  final User currentUser;
 
-  @override
-  State<HomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
+  final usersQuery =
+    FirebaseFirestore.instance.collection('users');
 
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
       appBar: AppBar(
- 
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
+        title: const Text('Users Collection'),
+        actions: [
+          IconButton(
+            onPressed: (){
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => const UserProfileScreen()));
+            }, 
+          icon: const Icon(Icons.person),
+          ),
+        ],
       ),
-      body: Center(
-
-        child: Column(
-  
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: FirestoreListView<Map<String, dynamic>>(
+        query: usersQuery,
+        itemBuilder: (context, snapshot) {
+          Map<String, dynamic> user = snapshot.data();
+          return Text('User name is ${user['displayName']}');
+        },
       ),
     );
   }
